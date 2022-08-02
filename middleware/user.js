070560c1df3be16b02623cd10d060d7980325293
mihsoldi <srlsoldi3@gmail.com>
2022-08-02 16:59:57 +0300

    Refactor user middleware

--- middleware/user.js
+++ middleware/user.js
@@ -2,6 +2,7 @@ export default async function({ store, redirect, app }) {
     const user = store.getters["near/getCurrentUser"]
     if(!user) {
         // console.log(store.getters["near/getCurrentUser"])
+        // console.log(store.getters["near/getCurrentUser"])
         return redirect('/')
     }
 }
